5379f306dc60309ea92f690171f4a109f7ee9bc4
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-02-18 18:38:35 +0100

    Creación de componentes
    He creado los componentes
    Login
    Registro
    Perfil
    
    También he configurado las rutas para ir de pagina en pagina

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { PerfilUsuario } from './perfil-usuario/perfil-usuario';
+import { Registro } from './registro/registro';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'login', //es lo que aparece en la URL.
+    component: Login, // Login es el componente que se mostrará
+  },
+  {
+    path: 'perfil',
+    component: PerfilUsuario,
+  },
+  {
+    path: 'registro',
+    component: Registro,
+  },
+  {
+  path: '',
+  redirectTo: 'login',
+  pathMatch: 'full' //Esto es para que ci no pones nada en la URL te rerdiriga a el login
+}
+
+];
---
+++ frontend/src/app/login/login.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  showPassword = false;
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+}
