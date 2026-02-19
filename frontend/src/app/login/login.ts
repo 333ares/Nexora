d7907c11ba3601fb235ac9f7d05d2ca50cc0a79a
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-02-19 16:10:17 +0100

    Cambios en el login frontend
    He estado haciendo toda la parte visual del inicio de sesión. (HTML y CSS).
    Ya esta casi acabado.

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit, OnDestroy {
+
   showPassword = false;
+
+  quotes = [
+    { text: '"El precio es lo que pagas, el valor es lo que recibes"', author: '– Warren Buffett' },
+    { text: '"No ahorres lo que te queda después de gastar, gasta lo que te queda después de ahorrar"', author: '– Warren Buffett' },
+    { text: '"La inversión en conocimiento paga el mejor interés"', author: '– Benjamin Franklin' },
+    { text: '"El dinero es un buen sirviente pero un mal amo"', author: '– Francis Bacon' },
+    { text: '"Nunca gastes tu dinero antes de ganarlo"', author: '– Thomas Jefferson' },
+  ];
+
+  currentIndex = 0;
+  private timer: any;
+
+  ngOnInit() {
+    this.timer = setInterval(() => this.next(), 4000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.quotes.length;
+  }
+
+  goTo(i: number) {
+    this.currentIndex = i;
+    clearInterval(this.timer);
+    this.timer = setInterval(() => this.next(), 4000);
+  }
 
   togglePassword() {
     this.showPassword = !this.showPassword;
