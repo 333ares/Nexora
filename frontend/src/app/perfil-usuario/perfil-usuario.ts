70b4c5b28eac4a4e7da6b4ef4f8e09f530dd62b1
DayronVarsendan <dayronvarsendan@gmail.com>
2026-02-20 16:58:47 +0100

    Frontend perfil 1
    Primer contacto con el frontend de la pagina de perfil, ahun queda pulirlo porque hay cosas que hay que ajustar y cosas que faltan todavia

--- frontend/src/app/perfil-usuario/perfil-usuario.ts
+++ frontend/src/app/perfil-usuario/perfil-usuario.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil-usuario',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,FormsModule],
   templateUrl: './perfil-usuario.html',
   styleUrl: './perfil-usuario.css',
 })
 export class PerfilUsuario {
+
+  user = {
+    nombre: 'Arn',
+    apellidos: 'Gómez',
+    username: 'arngomez',
+    email: 'arn@email.com'
+  };
+
+  transactions  = [
+    { tipo: 'ingreso', concepto: 'Ingreso nómina', cantidad: 2000 },
+    { tipo: 'gasto', concepto: 'Ocio', cantidad: 120 },
+    { tipo: 'gasto', concepto: 'Comida', cantidad: 80 }
+  ];
+
+
 
 }
