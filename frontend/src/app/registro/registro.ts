e121bca78a0d61949aed1e8866c3cba857383788
Daniel <dani.alcaraz.p@gmail.com>
2026-02-20 17:42:37 +0100

    Añadidos en TS del registro
    He añadido las funciones en el TS del registro para que sea posible la implementación del carrusel. Dichas funciones implementadas son las mismas que ya hay en login:
    - Para que pase automáticamente por cada frase
    - Para destruir el componente al cambiar de pagina
    - Para cambiar de frase
    - Para ir a una frase especifica

--- frontend/src/app/registro/registro.ts
+++ frontend/src/app/registro/registro.ts
@@ -9,5 +9,38 @@ import { RouterLink } from '@angular/router';
   styleUrl: './registro.css',
 })
 export class Registro {
+  quotes = [
+    { text: '"El precio es lo que pagas, el valor es lo que recibes"', author: '– Warren Buffett' },
+    { text: '"No ahorres lo que te queda después de gastar, gasta lo que te queda después de ahorrar"', author: '– Warren Buffett' },
+    { text: '"La inversión en conocimiento paga el mejor interés"', author: '– Benjamin Franklin' },
+    { text: '"El dinero es un buen sirviente pero un mal amo"', author: '– Francis Bacon' },
+    { text: '"Nunca gastes tu dinero antes de ganarlo"', author: '– Thomas Jefferson' },
+  ];
+
+  currentIndex = 0;
+  private timer: any;
+
+  ngOnInit() {
+    this.timer = setInterval(() => this.next(), 4000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.quotes.length;
+  }
+
+  goTo(i: number) {
+    this.currentIndex = i;
+    clearInterval(this.timer);
+    this.timer = setInterval(() => this.next(), 4000);
+  }
   
+  /*
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+  */
 }
