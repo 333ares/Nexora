98abc7052b23da62f4f2ad1039df4ff50175064c
DayronVarsendan <dayronvarsendan@gmail.com>
2026-02-24 16:14:18 +0100

    Revert "Error en la migracion de chunks"
    This reverts commit 59d2e0360a7bc1e5ad9110e6a686e63f97d15f8d.

--- backend/database/migrations/2026_02_16_151827_create_chunks_table.php
+++ backend/database/migrations/2026_02_16_151827_create_chunks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('chunks', function (Blueprint $table) {
             $table->id();
             $table->text('texto'); //String soporta hasta 255 caracteres, pero los chunks tienen mas de esa cifra.
-            $table->json('embedding'); //Porque contiene arrays de numeros tipo 0.123...
+            $texto->json('embedding'); //Porque contiene arrays de numeros tipo 0.123...
             $table->string('fuente');
             $table->timestamps();
         });
