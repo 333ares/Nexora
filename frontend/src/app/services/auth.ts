02edbde69a8da7e649e3643ccd6c59be58bc6b77
333ares <aresgp5335@gmail.com>
2026-02-25 15:40:20 +0100

    Creado el service de autenticación
    Se llama "auth", en este defino la ruta api predeterminada, uso las funciones de login y logout, entre otros.

---
+++ frontend/src/app/services/auth.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  private apiUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, {
+      email,
+      password
+    });
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/logout`, {});
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
+  }
+}
