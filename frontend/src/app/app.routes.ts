a08e614618f1636033c4d49c1c9684f04e0a5635
333ares <aresgp5335@gmail.com>
2026-02-26 19:00:07 +0100

    Añadido un guard
    Para la ruta de perfil por ejemplo, no quiero que un usuario acceda sin tener una sesión activa, así que lo he implementado.
    Además también he cambiado los márgenes del separador.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { PerfilUsuario } from './perfil-usuario/perfil-usuario';
 import { Registro } from './registro/registro';
-
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -12,15 +12,16 @@ export const routes: Routes = [
   {
     path: 'perfil',
     component: PerfilUsuario,
+    canActivate: [AuthGuard]
   },
   {
     path: 'registro',
     component: Registro,
   },
   {
-  path: '',
-  redirectTo: 'login',
-  pathMatch: 'full' //Esto es para que ci no pones nada en la URL te rerdiriga a el login
-}
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full' //Esto es para que ci no pones nada en la URL te rerdiriga a el login
+  }
 
 ];
