18c0adb93e6389da52c7b16d753a2caec368d1f4
333ares <aresgp5335@gmail.com>
2026-03-03 15:52:13 +0100

    Añadir movimiento
    He creado el método de añadir movimiento y me he basado en el de registrar a un usuario.

--- backend/app/Http/Controllers/MovimientosController.php
+++ backend/app/Http/Controllers/MovimientosController.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movimientos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Validator;
 
-class Movimientos extends Controller
+class MovimientosController extends Controller
 {
     public function apuntarMovimiento(Request $request)
     {
-        
+        $validator = Validator::make($request->all(), [
+            'tipo' => 'required|in:ingreso,gasto',
+            'cantidad' => 'required|decimal:2',
+            'categoria' => 'required|string',
+            'descripcion' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'error',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $movimiento = Movimientos::create([
+            'tipo' => $request->tipo,
+            'cantidad' => $request->cantidad,
+            'categoria' => $request->categoria,
+            'descripcion' => $request->descripcion,
+            'fecha' => Carbon::now(),
+            'usuario_id' => $request->user()->id,
+        ]);
+
+        if ($movimiento) {
+            return response()->json([
+                'message' => 'Movimiento añadido correctamente'
+            ], 201);
+
+            // Si no, mostramos el error
+        } else {
+            return response()->json([
+                'message' => 'error',
+                'errors' => $validator->errors()
+            ], 400);
+        }
     }
 }
