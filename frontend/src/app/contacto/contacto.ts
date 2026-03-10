7d5b4488f55b2ae37f6b3eb6e5518e2e65c8451c
Daniel <dani.alcaraz.p@gmail.com>
2026-03-10 16:13:15 +0100

    Frontend contacto modificaciones
    Se han modificado algunos aspectos estéticos del contacto, eliminando así la opción de iniciar sesión, que no tiene sentido en este contexto.

--- frontend/src/app/contacto/contacto.ts
+++ frontend/src/app/contacto/contacto.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './contacto.css',
 })
 export class Contacto {
-
+  // Añade esto para solucionar el error TS2339
+  onSubmit(event: any) {
+    event.preventDefault(); // Evita que la página se recargue
+    console.log('Formulario enviado');
+    // Aquí irá la lógica para conectar con tu API de Laravel más adelante
+  }
 }
