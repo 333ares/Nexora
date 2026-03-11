c185b1b04d6fc6e45303c7562d8dbef8df49fea3
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-03-11 18:03:25 +0100

    Reestructuración de rutas hijas de movimientos
    Creación y estructuración de los componentes hijos de movimientos (Resumen, Lista, Calendario, Estadísticas, Retos), corrección de rutas con implementación de ruta padre e hijas, cada hijo con su HTML estático listo para conectar con backend, todo el CSS centralizado en el padre usando ViewEncapsulation.None para que los estilos lleguen a los hijos
    
    Creacion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { Registro } from './registro/registro';
 import { AuthGuard } from './guards/auth-guard';
 import { Planes } from './planes/planes';
 import { Movimientos } from './movimientos/movimientos';
+import { Calendario } from './movimientos/calendario/calendario';
+import { Estadisticas } from './movimientos/estadisticas/estadisticas';
+import { Retos } from './movimientos/retos/retos';
+import { Lista } from './movimientos/lista/lista';
+import { Resumen } from './movimientos/resumen/resumen';
 
 export const routes: Routes = [
   {
@@ -25,18 +30,24 @@ export const routes: Routes = [
     component: Planes,
     canActivate: [AuthGuard]
   },
+
   {
     path: 'movimientos',
     component: Movimientos,
-    canActivate: [AuthGuard]
+    children: [
+      { path: 'resumen', component: Resumen },
+      { path: 'lista', component: Lista },
+      { path: 'calendario', component: Calendario },
+      { path: 'estadisticas', component: Estadisticas },
+      { path: 'retos', component: Retos },
+      { path: '', redirectTo: 'resumen', pathMatch: 'full' }
+    ]
   },
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full' //Esto es para que si no pones nada en la URL te rerdiriga a el login
   }
-
-
 
 
 ];
