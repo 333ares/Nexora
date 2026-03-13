5b28c49e480bfa6be9c03c2cce3c95030e97ce40
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2026-03-13 16:47:11 +0100

    Academia
    Rutas hijas creadas
    Y primeros retoques academia html y css

---
+++ frontend/src/app/academia-home/academia-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academia-home',
+  imports: [],
+  templateUrl: './academia-home.html',
+  styleUrl: './academia-home.css',
+})
+export class AcademiaHome {
+
+}
---
+++ frontend/src/app/ver-video/ver-video.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-video',
+  imports: [],
+  templateUrl: './ver-video.html',
+  styleUrl: './ver-video.css',
+})
+export class VerVideo {
+
+}
