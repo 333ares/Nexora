545943d80a7c70f1d99c344e4e9c3328163ed666
Daniel <dani.alcaraz.p@gmail.com>
2026-03-13 16:58:54 +0100

    Logo menu superior "interno" linkeable
    En el menu superior de academia - movimientos - perfil, al hacer clic en el logo o las letras hace una redirección a inicio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -40,7 +40,7 @@ export const routes: Routes = [
   {
     path: 'movimientos',
     component: Movimientos,
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
     children: [
       { path: 'resumen', component: Resumen },
       { path: 'lista', component: Lista },
