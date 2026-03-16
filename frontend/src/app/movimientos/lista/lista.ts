4a99fa45ecaca9729b4ddcf11c577f320527974c
333ares <aresgp5335@gmail.com>
2026-03-16 15:30:22 +0100

    historial de moviminetos
    Ahora lo he implementado en la pantalla de lista.

--- frontend/src/app/movimientos/lista/lista.ts
+++ frontend/src/app/movimientos/lista/lista.ts
@@ -11,6 +11,8 @@ import { Auth } from '../../services/auth';
 })
 export class Lista implements OnInit {
   movimientos: any[] = [];
+  movimientosFiltrados: any[] = [];
+  filtroActivo: string = 'todos';
 
   constructor(private authService: Auth, private cdr: ChangeDetectorRef) { }
 
@@ -22,15 +24,27 @@ export class Lista implements OnInit {
     this.authService.getHistorialMovimientos().subscribe({
       next: (response) => {
         this.movimientos = response.movimientos;
+        this.movimientosFiltrados = this.movimientos;
         this.cdr.detectChanges();
       },
       error: (err) => {
         if (err.status === 400) {
           this.movimientos = [];
+          this.movimientosFiltrados = [];
         } else {
           console.error('Error al obtener el historial:', err);
         }
       }
     });
+  }
+
+  filtrar(tipo: string): void {
+    this.filtroActivo = tipo;
+    if (tipo === 'todos') {
+      this.movimientosFiltrados = this.movimientos;
+    } else {
+      this.movimientosFiltrados = this.movimientos.filter(m => m.tipo === tipo);
+    }
+    this.cdr.detectChanges();
   }
 }
