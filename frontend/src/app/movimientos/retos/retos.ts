b4f9fc1aa38ae2a1e013162a4afbe7ba5abeda79
Daniel <dani.alcaraz.p@gmail.com>
2026-03-19 16:14:24 +0100

    Conexión retos frontend con backend
    Se ha creado un archivo llamado retos.service.ts para conectar la APIs de retos en el backend

--- frontend/src/app/movimientos/retos/retos.ts
+++ frontend/src/app/movimientos/retos/retos.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RetosService } from '../../services/retos.service'; // Subimos dos niveles
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-retos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './retos.html',
   styleUrl: './retos.css'
 })
-export class Retos {}
+export class Retos implements OnInit {
+  listaDeRetos: any[] = [];
+
+  constructor(private retosService: RetosService) {}
+
+  ngOnInit(): void {
+    this.retosService.getRetos().subscribe({
+      next: (data) => {
+        this.listaDeRetos = data;
+        console.log('Retos cargados: ', data);
+      },
+      error: (err) => console.error('Fallo al conectar con Laravel', err)
+    });
+  }
+}
