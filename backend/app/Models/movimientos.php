42dbc8e9d4b6b6f96a2a2947bec625d6aa34891a
333ares <aresgp5335@gmail.com>
2026-04-10 18:46:16 +0200

    Imports del faker

--- backend/app/Models/movimientos.php
+++ backend/app/Models/movimientos.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Movimientos extends Model
 {
+    use HasFactory, Notifiable;
+
     protected $fillable = [
         'id',
         'tipo',
