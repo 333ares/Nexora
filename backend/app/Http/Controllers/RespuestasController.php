16e61f53270cc1176fc1c1191734dbe77a9d77c0
333ares <aresgp5335@gmail.com>
2026-04-24 17:52:39 +0200

    Controlador de respuestas creado
    y borrado el de visualizaciones

---
+++ backend/app/Http/Controllers/RespuestasController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Respuesta;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RespuestasController extends Controller
+{
+    
+}
