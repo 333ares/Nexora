c3981ab77eafbb8f6146ba7114d0d582792d796f
333ares <aresgp5335@gmail.com>
2026-04-27 16:04:17 +0200

    función de añadir respuestas a foros
    Muy sencilla, como la de crear foro casi (la lógica es la misma pero con los campos cambiados).

--- backend/app/Http/Controllers/RespuestasController.php
+++ backend/app/Http/Controllers/RespuestasController.php
@@ -8,5 +8,29 @@ use Illuminate\Support\Facades\Validator;
 
 class RespuestasController extends Controller
 {
-    
+    public function responderForo(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'IDforo' => 'required|integer',
+            'respuesta' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'error',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $respuesta = Respuesta::create([
+            'IDforo' => $request->IDforo,
+            'respuesta' => $request->respuesta,
+            'IDusuario' => $request->user()->IDusuario
+        ]);
+
+        return response()->json([
+            'message' => 'Respuesta añadida correctamente',
+            'respuesta' => $respuesta
+        ], 201);
+    }
 }
