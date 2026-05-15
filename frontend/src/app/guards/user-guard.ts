e1a6b5c8520e96f61ce3059603510ebe2ea188b8
333ares <aresgp5335@gmail.com>
2026-05-15 17:03:24 +0200

    Guards comentados

--- frontend/src/app/guards/user-guard.ts
+++ frontend/src/app/guards/user-guard.ts
@@ -7,12 +7,15 @@ export class UserGuard implements CanActivate {
   constructor(private auth: Auth, private router: Router) { }
 
   canActivate(): boolean {
+    // Miramos si el usuario esta logueado
     const usuario = this.auth.getUsuario();
 
+    // Si el usuario no es admin (id=1), puede acceder a la ruta
     if (Number(usuario?.id) !== 1) {
       return true;
     }
 
+    // Si el usuario es admin, lo redirigimos al panel de administración
     this.router.navigate(['/panel-admin']);
     return false;
 
